175ca00661d7564e644e5556b928be7fea30f425
yichuang <315353100@qq.com>
2015-12-14 13:54:25 +0800

    测试git上传

---
+++ CloudTest/CloudTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudTest
+//
+//  Created by youkia on 15/12/14.
+//  Copyright © 2015年 YK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
